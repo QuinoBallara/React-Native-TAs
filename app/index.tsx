dea9aa927837e930406436555b1a7569ffc34fe5
Quino <joaquinballara02@gmail.com>
2024-10-30 20:31:49 -0300

    casi ta2

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,48 @@
 import { Text, View } from "react-native";
+import { useState } from "react";
+import { TextInput } from "react-native";
+import { StyleSheet } from "react-native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function Index() {
+  const [text, setInput] = useState("");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+
+    text: {
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    box: {
+      width: 100,
+      height: 100,
+    }
+  });
+
+  const handleSetInput = (text: string) => {
+    console.log(text);
+    setInput(text);
+  }
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View style={styles.container}>
+        <TextInput style={[styles.box, styles.input]} value={text} onChangeText={handleSetInput} editable />
+        <Text style={[styles.text, styles.box]}>{text}</Text>
+      </View>
+    </GestureHandlerRootView>
   );
 }
