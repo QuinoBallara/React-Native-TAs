592028cf3d52d3c67e50a817f0ed2a24188bd2ed
Quino <joaquinballara02@gmail.com>
2024-11-04 17:00:38 -0300

    fin TA4

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,29 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Image, Pressable, Text, View } from "react-native";
+import { GestureHandlerRootView, NativeViewGestureHandler, TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Index() {
+  const [image, setImage] = useState(true)
+
   return (
-    <View
+    <GestureHandlerRootView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Image source={{ uri: "https://picsum.photos/200/300" }} style={{ width: 400, height: 400 }} />
+      <TouchableOpacity onPress={() => setImage(!image)} style={{
+        backgroundColor: "blue",
+        padding: 10,
+        marginTop: 10,
+      }} >
+        <Text style={{
+          color: "white",
+          fontSize: 20,
+        }}>Change image</Text>
+      </TouchableOpacity>
+    </GestureHandlerRootView>
   );
 }
