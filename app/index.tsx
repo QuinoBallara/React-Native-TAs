4c57db5ad3bea5581be9ac47bf9fb08c5a5502e2
Quino <joaquinballara02@gmail.com>
2024-11-08 09:34:08 -0300

    fin TA11

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,59 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { GestureHandlerRootView, TouchableOpacity } from "react-native-gesture-handler";
 
 export default function Index() {
+  const [image, setImage] = useState<string>();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  }
+
+  const styles = StyleSheet.create({
+    button: {
+      backgroundColor: "blue",
+      padding: 15,
+      borderRadius: 5,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 20,
+    },
+    title: {
+      fontSize: 24,
+      marginBottom: 20,
+    },
+    container: {
+      flex: 1,
+      justifyContent: "flex-start",
+      alignItems: "center",
+      gap: 10
+    }
+  })
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+    <GestureHandlerRootView
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <Text style={styles.title}>Choose your favorite photo:</Text>
+      <TouchableOpacity style={styles.button} onPress={pickImage}>
+        <Text style={styles.buttonText}>Choose photo</Text>
+      </TouchableOpacity>
+      {image && (
+        <View>
+          <Image source={{ uri: image }} style={{ width: 500, height: 500 }} />
+        </View>
+      )}
+    </GestureHandlerRootView>
   );
 }
